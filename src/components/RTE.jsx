8dbc38aa3bf24a7ecd65464a5f7a4b8d69c310c1
Ayush Patel <aaushpatel009@gmail.com>
2025-01-22 14:09:12 +0530

    initial commit

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -13,6 +13,7 @@ export default function RTE({name, control, label, defaultValue =""}) {
     control={control}
     render={({field: {onChange}}) => (
         <Editor
+        apiKey = 'y4zairpcfn4qnd1hwv1nc4l57kg0wf1sy3yn0fh17e8birp9'
         initialValue={defaultValue}
         init={{
             initialValue: defaultValue,
